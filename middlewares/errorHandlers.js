a9349dda286e4a25903fd06a495ce97c1447c8d0
Muhammad Mushlih Al 'Aadil <mmushlih.alaadil@gmail.com>
2024-08-19 09:25:30 +0700

    Error Handling: Done

--- middlewares/errorHandlers.js
+++ middlewares/errorHandlers.js
@@ -4,6 +4,16 @@ function errorHandler (err, req, res, next) {
         res.status(400).json({message: err.errors.map((err) => err.message)})
     } else if (err.name === 'SequelizeUniqueConstraintError') {
         res.status(400).json({message: err.errors.map((err) => err.message)})
+    } else if (err.name === 'PenalizedMember') {
+        res.status(400).json({message: 'You are being penalized, please return within 3 days to borrow the book again.' })
+    } else if (err.name === 'MaximumLimit') {
+        res.status(400).json({message: 'You already borrowed 2 books, please return 1 book so you can borrow another' })
+    } else if (err.name === 'NotAvailable') {
+        res.status(400).json({message: 'Book already borrowed by another member, chose another' })
+    } else if (err.name === 'NotBorrowed') {
+        res.status(400).json({message: 'You are not borrowing that book' })
+    } else if (err.name === 'MemberNotFound') {        
+        res.status(404).json({message: "Member is not found" })
     } else {
         res.status(500).json({message: `Internal Server Error`})
     }
